841a531a596c15add854f4089a5f2b0b8e3d1c5b
emiliano jossie <jossie@tennet.id>
2023-11-30 09:59:46 +0700

    hahahaahha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,26 +2,29 @@
 
 @section('content')
 <div class="max-w-screen-xl flex flex-col items-center justify-between mx-auto p-20">
-    <h1 class="text-black text-5xl text-center">
+    <h1 class="text-black text-5xl text-center border mb-2">
         Login
     </h1>
-    <form class="card card-md" action="#" method="post" autocomplete="off">
+    <form class="card card-md" action="{{ route('login.post') }}" method="post" autocomplete="off">
         @csrf
 
         <div class="card-body flex flex-col">
-            <h2 class="card-title text-center mb-4">Login</h2>
 
-            <div class="mb-3">
+            <div class="mb-3 flex flex-col space-y-2">
                 <label class="form-label">Username or Email</label>
-                <input type="email" name="email" value="" class="form-control @error('email') is-invalid @enderror" placeholder="Email" required autofocus tabindex="1">
+                <input type="email" name="email" value="" class="form-control @error('email') is-invalid @enderror w-80 rounded-md h-11 bg-gray-300" placeholder="Email" required autofocus tabindex="1">
             </div>
 
-            <div class="mb-3">
+            <div class="mb-3 flex flex-col">
                 <label class="form-label">
-                    Masukkan Password
+                    Password
                 </label>
-                <input type="password" name="password" class="form-control" placeholder="Password" required tabindex="2">
+                <input type="password" name="password" class="form-control w-80 rounded-md h-11 bg-gray-300" placeholder="Password" required tabindex="2">
             </div>
+        </div>
+        <div class="mt-3 flex flex-row justify-between">
+            <div class="mt-2">Forgot Password? </div>
+            <button class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn bg-gray-300 rounded-md h-11 w-32" type="submit">Login</button>
         </div>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/login', function () {
-    return view('auth.login');
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/login',[LoginController::class,'showLoginForm'])->name('login');
+    Route::post('/login',[LoginController::class,'login'])->name('login.post');
+    Route::get('/register',[RegisterController::class,'showRegisterForm'])->name('register');
+    Route::post('/register',[RegisterController::class,'register'])->name('register.post');
+    Route::get('/', function () {
+        return view('index');
+    })->name('home');
+    
 });
